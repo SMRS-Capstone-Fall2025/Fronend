800a36593167199165f5cd90d4c620a08a7fedc7
buidat1209 <datnbde140193@fpt.edu.vn>
2025-11-17 15:13:31 +0700

    update code

---
+++ src/page/admin/projects.tsx
@@ -0,0 +1,10 @@
+import AdminLayout from "./layout";
+import { DeanProjectsContent } from "../dean/projects";
+
+export default function AdminProjectsPage() {
+  return (
+    <AdminLayout>
+      <DeanProjectsContent />
+    </AdminLayout>
+  );
+}
