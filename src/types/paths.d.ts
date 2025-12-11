11d69e0973c9a62cfbf5aa1489cbd382f0de2840
buidat1209 <datnbde140193@fpt.edu.vn>
2025-12-11 16:11:30 +0700

    update/page/councils

---
+++ src/types/paths.d.ts
@@ -0,0 +1,11 @@
+
+
+declare module "virtual:generated-pages";
+
+declare module "@/*";
+
+declare module "@/components/*";
+declare module "@/components/ui/*";
+declare module "@/lib/*";
+declare module "@/app/*";
+declare module "@/pages/*";
