138adcb50da34fd97bd64a12fad52062933dd123
buidat1209 <datnbde140193@fpt.edu.vn>
2025-12-19 15:29:56 +0700

    index

---
+++ src/services/types/index.ts
@@ -0,0 +1,18 @@
+export * from "./common";
+export * from "./account";
+export * from "./upload";
+export * from "./task";
+export * from "./project";
+export * from "./milestone";
+export * from "./project-member";
+export * from "./council";
+export type {
+  GetAllCouncilsQueryParams,
+  PageResponseCouncilResponse,
+  CouncilPageResponse,
+} from "./council";
+export * from "./major";
+export type { LecturerDto, LecturerListResponse } from "./major";
+export * from "./grading";
+export * from "./plagiarism";
+export * from "./publication";
